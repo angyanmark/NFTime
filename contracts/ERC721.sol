d6ddf77f80d25529fed6ea3df0dea7677a9d6657
Ángyán Márk <angyanmark97@gmail.com>
2021-05-07 21:15:11 +0200

    implement ERC721 interface

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -47,8 +47,8 @@ interface ERC721 /* is ERC165 */ {
     /// @param _from The current owner of the NFT
     /// @param _to The new owner
     /// @param _tokenId The NFT to transfer
-    /// @param data Additional data with no specified format, sent in call to `_to`
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) external payable;
+    /// @param _data Additional data with no specified format, sent in call to `_to`
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata _data) external payable;
 
     /// @notice Transfers the ownership of an NFT from one address to another address
     /// @dev This works identically to the other function with an extra data parameter,
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,7 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract NFT {
+import "./ERC721.sol";
+
+contract NFT is ERC721 {
   constructor() {
+  }
+
+  function balanceOf(address _owner) public override view returns (uint256) {
+    // TODO
+  }
+
+  function ownerOf(uint256 _tokenId) public override view returns (address) {
+    // TODO
+  }
+
+  function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata _data) public override payable {
+    // TODO
+  }
+
+
+  function safeTransferFrom(address _from, address _to, uint256 _tokenId) public override payable {
+    // TODO
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) public override payable {
+    // TODO
+  }
+
+  function approve(address _approved, uint256 _tokenId) public override payable {
+    // TODO
+  }
+
+  function setApprovalForAll(address _operator, bool _approved) public override {
+    // TODO
+  }
+
+  function getApproved(uint256 _tokenId) public override view returns (address) {
+    // TODO
+  }
+
+  function isApprovedForAll(address _owner, address _operator) public override view returns (bool) {
+    // TODO
   }
 }
