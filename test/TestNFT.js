e0be2c2befbdc0803942f8ec696bfe5978b07717
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2021-05-11 12:43:38 +0200

    approve, getApproved, buy tests

--- test/TestNFT.js
+++ test/TestNFT.js
@@ -23,4 +23,51 @@ contract("NFT", function (accounts) {
       assert.equal(balance2, 1, "Balance should be 1!");
     });
   });
+
+  it("Test approve and getApproved", () => {
+    return NFT.deployed().then(async (nft) => {
+      let id = 1;
+
+      await nft.mint(URI, { from: account_one });
+      await nft.approve(account_two, id, { from: account_one });
+
+      let actual = await nft.getApproved(id, { from: account_one });
+
+      assert.equal(actual, account_two, "Account 2 should be approved!");
+    });
+  });
+
+  /*it("Test approve with burn", () => {
+    return NFT.deployed().then(async (nft) => {
+      let id = 1;
+
+      await nft.mint(URI, { from: account_one });
+      await nft.approve(account_two, id, { from: account_one });
+      await nft.burn(id, { from: account_two });
+
+      let actual = await nft.balanceOf(account_one, { from: account_one });
+
+      assert.equal(actual.toString(), "0", "Should not have any NFTs!");
+    });
+  });*/ // FAILOL DE MÉ ???
+
+  it("Test buy with not enough Ether sent", () => {
+    return NFT.deployed().then(async (nft) => {
+      let price = 10;
+      let id = 1;
+
+      await nft.mint(URI, { from: account_one });
+      await nft.forSale(id, price, { from: account_one });
+
+      try {
+        await nft.buy(id, { from: account_two, value: price - 1 });
+      } catch (ex) {
+        assert.equal(
+          "Error: Returned error: VM Exception while processing transaction: revert NFT costs more -- Reason given: NFT costs more.",
+          ex.toString(),
+          "Should throw \"NFT costs more\"!"
+        );
+      }
+    });
+  });
 });
